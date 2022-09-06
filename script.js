6a176ac39ed05b8ea3769d35fdbef46ed87da4d5
Andrea <andr45f9@stud.kea.dk>
2022-09-06 10:02:50 +0200

    function setup

---
+++ script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const HTML = [];
+
+document.addEventListener("DOMContentLoaded", start);
+
+function start() {}
+
+//Gets the selected color form the input field and reads it in the different color values.
+function getSelectedColor() {}
+
+//Convert the hex value to rgb.
+function convertToRGB(hex) {}
+
+//Convert the hex value to hsl.
+function convertToHSL(hex) {}
+
+//Displays the color in the different color values aswell as showing the color in the color circle.
+function displayAllValues() {}
